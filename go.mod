fed48afba539cf1d7a9b4a193e053640a9c1baba
shed909 <shane@shanehull.com.au>
2023-12-14 16:57:04 +1100

    init

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/shanehull/quotes-api
+
+go 1.21.3
+
+toolchain go1.21.4
+
+require (
+	github.com/a-h/templ v0.2.476
+	github.com/rs/zerolog v1.31.0
+	github.com/syumai/workers v0.18.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/syumai/workers"
+)
+
+var globalRand = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func cors(w http.ResponseWriter, req *http.Request) {
+	w.Header().Add("Access-Control-Allow-Origin", "*")
+	w.Header().Add("Access-Control-Allow-Methods", "GET")
+	w.Header().Add("Access-Control-Allow-Headers", "Content-Type, hx-current-url, hx-request, hx-trigger, hx-target")
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
+
+	if req.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusNoContent)
+		return
+	}
+}
+
+func quoteHandler(w http.ResponseWriter, req *http.Request) {
+	cors(w, req)
+
+	idx := globalRand.Intn(len(quotes))
+
+	component := quote(quotes[idx].Text, quotes[idx].Author)
+
+	buf := new(bytes.Buffer)
+	defer buf.Reset()
+
+	if err := component.Render(req.Context(), buf); err != nil {
+		log.Printf("failed to render component:", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	_, err := w.Write(buf.Bytes())
+	if err != nil {
+		log.Printf("failed to write response:", err)
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+		return
+	}
+}
+
+func main() {
+	http.HandleFunc("/quote", quoteHandler)
+
+	workers.Serve(nil)
+}
